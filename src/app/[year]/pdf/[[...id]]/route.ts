37a91c87f776c4587b435fa0b4d413f086d85bae
Charles Josh <cjoshua.alba@gmail.com>
2025-02-10 22:01:45 +0800

    Checkpoint: Make PDF handle some edge cases

--- src/app/[year]/pdf/[[...id]]/route.ts
+++ src/app/[year]/pdf/[[...id]]/route.ts
@@ -6,10 +6,10 @@ export async function GET(
   { params }: { params: Promise<{ year: string; id: string[] }> }
 ) {
   const { year, id: idArray } = await params;
-  const id = idArray[0];
+  const id = idArray?.[0] ?? "";
   const doc = await PDFDocument.create();
 
-  const imageRaw = await fetch(`${BASE_URL}/${year}/png/${id}`);
+  const imageRaw = await fetch(`${BASE_URL}/${year}/png/${idArray?.join("/")}`);
   const imageBuffer = await imageRaw.arrayBuffer();
   const imageInPage = await doc.embedPng(imageBuffer);
   const imageDims = imageInPage.scale(0.7);
@@ -27,7 +27,7 @@ export async function GET(
   return new Response(pdf, {
     headers: {
       "Content-Type": "application/pdf",
-      "Content-Disposition": `inline; filename= "kodigo-me-${id}.pdf"`,
+      "Content-Disposition": `inline; filename= "kodigo-me${id ? "-" + id : ""}.pdf"`,
       "Cache-Control": `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`,
     },
   });
