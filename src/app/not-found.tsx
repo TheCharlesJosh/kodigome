d62140740133607d30ec8472b9d7a863a426588f
Charles Josh <cjoshua.alba@gmail.com>
2025-02-12 01:03:39 +0800

    Revert changes to NotFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,11 @@
 import { generatePageMeta } from "@/lib/meta";
-import dynamic from "next/dynamic";
+import NotFound from "./not-found/not-found";
 
 export const metadata = generatePageMeta({
   title: "Page Not Found",
   description: "Page Not Found",
 });
 
-const DynamicNotFound = dynamic(() => import("./not-found/not-found"), {
-  ssr: false,
-});
-
 export default function Page() {
-  return <DynamicNotFound />;
+  return <NotFound />;
 }
