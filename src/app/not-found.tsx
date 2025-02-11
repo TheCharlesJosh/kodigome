c1df7d67a88b9c11ae4617e1c9dc6040e675abd0
Charles Josh <cjoshua.alba@gmail.com>
2025-02-12 00:58:25 +0800

    Change NotFound component to dynamic to fix vercel build issues

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,15 @@
 import { generatePageMeta } from "@/lib/meta";
-import NotFound from "./not-found/not-found";
+import dynamic from "next/dynamic";
 
 export const metadata = generatePageMeta({
   title: "Page Not Found",
   description: "Page Not Found",
 });
 
+const DynamicNotFound = dynamic(() => import("./not-found/not-found"), {
+  ssr: false,
+});
+
 export default function Page() {
-  return <NotFound />;
+  return <DynamicNotFound />;
 }
